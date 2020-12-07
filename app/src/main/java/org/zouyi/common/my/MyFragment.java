d6c5a30cb1c630fd7fd88a20cf3084df5e5dfbce
zouyi777 <18215626820@163.com>
2020-12-07 19:26:01 +0800

    新增调用古诗生成模型接口

--- app/src/main/java/org/zouyi/common/my/MyFragment.java
+++ app/src/main/java/org/zouyi/common/my/MyFragment.java
@@ -1,5 +1,6 @@
 package org.zouyi.common.my;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.zouyi.common.BaseFragment;
 import org.zouyi.common.R;
@@ -32,6 +34,7 @@ public class MyFragment extends BaseFragment implements View.OnClickListener {
     private View view;
     private EditText etMy;
     private Button btnRequest;
+    private TextView tvDispaly;
 
     public static MyFragment newInstance(String data) {
         Bundle args = new Bundle();
@@ -48,6 +51,7 @@ public class MyFragment extends BaseFragment implements View.OnClickListener {
         btnRequest = view.findViewById(R.id.btn_request);
         btnRequest.setOnClickListener(this);
         tvTitle.setText(getArguments().getString("key"));
+        tvDispaly = view.findViewById(R.id.tv_dispaly);
         flContent.addView(view);
         return baseView;
     }
@@ -86,12 +90,19 @@ public class MyFragment extends BaseFragment implements View.OnClickListener {
 
     //使用Retrofit+Rx从服务器加载数据
     private void loadDataByRetrofitRx(){
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://120.24.147.111:80")
+        String baseUrl = "http://120.24.147.111:80";
+//        String baseUrl = "http://10.5.9.57:5003";
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(baseUrl)
                 .addConverterFactory(new ToStringConverterFactory())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())//支持RxJava
                 .build();
         RetrofitService service = retrofit.create(RetrofitService.class);
-        Observable<String> observable =  service.getHelloRx();
+        String userName = etMy.getText().toString();
+//        Observable<String> observable =  service.getHelloRxGet(userName);
+//        Observable<String> observable =  service.getHelloRxPost(userName);
+        Observable<String> observable =  service.getVenala2ancnt(userName);
+        final ProgressDialog progressDialog = ProgressDialog.show(getActivity(),"","加载中……",
+                true, false);
         //RxAndroid其实就是对RxJava的扩展。比如AndroidSchedulers主线程在RxJava中就没有，因此要使用的话就必须得引用RxAndroid
         observable.subscribeOn(Schedulers.io())//请求数据的事件发生在io线程
                   .observeOn(AndroidSchedulers.mainThread())//请求完成后在主线程更显UI
@@ -99,15 +110,16 @@ public class MyFragment extends BaseFragment implements View.OnClickListener {
                     @Override
                     public void onCompleted() {
                         //所有事件都完成，可以做些操作。。。
+                        progressDialog.dismiss();
                     }
                     @Override
                     public void onError(Throwable e) {
                         e.printStackTrace(); //请求过程中发生错误
-                        etMy.setText(e.getMessage());
+                        Toast.makeText(getActivity(),e.getMessage(),Toast.LENGTH_LONG).show();
                     }
                     @Override
                     public void onNext(String book) {
-                        etMy.setText(book);
+                        tvDispaly.setText(book);
                     }
                 });
     }
--- app/src/main/java/org/zouyi/common/net/RetrofitService.java
+++ app/src/main/java/org/zouyi/common/net/RetrofitService.java
@@ -1,7 +1,11 @@
 package org.zouyi.common.net;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 import rx.Observable;
 
 public interface RetrofitService {
@@ -12,4 +16,22 @@ public interface RetrofitService {
     //使用了Retrofit+RxJava
     @GET("/hello")
     Observable<String> getHelloRx();
+
+    //使用了Retrofit+RxJava的Get请求
+    @GET("/hello_get")
+    Observable<String> getHelloRxGet(@Query("username")String username);
+
+    //使用了Retrofit+RxJava的Post请求
+    @POST("/hello_post")
+    Observable<String> getHelloRxPost(@Query("username")String username);
+
+    //使用了Retrofit+RxJava的Post请求
+    @FormUrlEncoded
+    @POST("/hello_post_form")
+    Observable<String> getHelloRxPostForm(@Field("username")String username);
+
+    //使用了Retrofit+RxJava的Post请求古诗预测接口
+    @FormUrlEncoded
+    @POST("/venala2ancnt")
+    Observable<String> getVenala2ancnt(@Field("seedwords")String seedwords);
 }
